3de49c8e762666bc968deeba28e9da75ffd59319
wangJunBo <wangJunBo@qq.com>
2016-06-15 15:29:17 +0800

    设置程序目录结构/设置启动控制器

---
+++ 网易新闻/网易新闻/Classes/Main/WYMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYMainViewController.h
+//  网易新闻
+//
+//  Created by 王军波 on 16/6/15.
+//  Copyright © 2016年 王军波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYMainViewController : UITabBarController
+
+@end
